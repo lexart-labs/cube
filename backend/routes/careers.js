9bdb4484d243956303d4bbf53603995ec73dfe8f
lucas-paz <lucas.hpaz@outlook.com>
2021-11-23 17:54:16 -0300

    Muestra info del puesto feat: creates route to get all items

---
+++ backend/routes/careers.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const Career 	 = require('../services/careers.service');
+
+router.get('/careers', async (_req, res) => {
+  const response = await Career.getAll();
+
+  res.set(['Content-Type', 'application/json']);
+  return res.send(response);
+});
+
+module.exports = router;
