4103c5a71e962230c290d6cdbba13db36b9e7c53
lucas-paz <lucas.hpaz@outlook.com>
2021-11-25 18:41:05 -0300

    Muestra info del puesto  feat: add logic to show the history of charges

--- webapp/app/components/timeline/timeline.component.js
+++ webapp/app/components/timeline/timeline.component.js
@@ -8,17 +8,41 @@ const Timeline = Vue.component('timeline-component', function (callback) {
 			mounted: function () {
 				axios.get('http://localhost:3001/careers')
 					.then(({ data: { response: careers } }) => {
+						const current = 'Jr. Software Architect';
+						let isFull = true;
+
 						am4core.useTheme(am4themes_animated);
 						const chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
 
-						const bulletLabels = careers.map(({ position }, i) => ({
-							'x': `${i + 1}`,
-							'y': 1,
-							'text': position,
-							'center': i % 2 === 0 ? 'top' : 'bottom'
-						}));
+						const data = careers.reduce((acc, cur, i) => {
+							const docTemplate = {
+								x: i++,
+								y: 1,
+								text: cur.position,
+								center: i % 2 === 0 ? 'top' : 'bottom',
+								config: {
+									fill: isFull ? '#2bc4a7' : 'white',
+								},
+							};
 
-						chart.data = bulletLabels;
+							if (cur.position === current) {
+								isFull = false;
+								return [...acc, { ...docTemplate, config: { fill: '#2bc4a7' } }]
+							}
+							return [...acc, docTemplate];
+						}, []);
+
+						// const bulletLabels = careers.map(({ position }, i) => ({
+						// 	'x': `${i + 1}`,
+						// 	'y': 1,
+						// 	'text': position,
+						// 	'center': i % 2 === 0 ? 'top' : 'bottom',
+						// 	'config': {
+						// 		'fill': position === current ? '#2bc4a7' : 'white',
+						// 	}
+						// }));
+
+						chart.data = data;
 
 						let xAxis = chart.xAxes.push(new am4charts.CategoryAxis());
 						xAxis.dataFields.category = 'x';
@@ -43,6 +67,7 @@ const Timeline = Vue.component('timeline-component', function (callback) {
 
 						let bullet = series.bullets.push(new am4charts.CircleBullet());
 						bullet.circle.fill = am4core.color('#2bc4a7');
+						bullet.circle.configField = 'config';
 						bullet.circle.radius = 10;
 
 						let labelBullet = series.bullets.push(new am4charts.LabelBullet());
