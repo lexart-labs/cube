0767b44f51066e2014efee96de06397463187a5f
lucas-paz <lucas.hpaz@outlook.com>
2021-11-26 10:29:41 -0300

    Muestra info del puesto  feat: remaining endpoints to crud at levels table

--- backend/routes/levels.js
+++ backend/routes/levels.js
@@ -9,4 +9,32 @@ router.get('/', async (_req, res) => {
   return res.send(response);
 });
 
+router.put('/:id', async (req, res) => {
+  const { level, active } = req.body;
+  const { id } = req.params;
+
+  const response = await Level.upsert(id, level, active);
+
+  res.set(['Content-Type', 'application/json']);
+  return res.send(response);
+});
+
+router.post('/', async (req, res) => {
+  const { level, active } = req.body;
+
+  const response = await Level.upsert(null, level, active);
+
+  res.set(['Content-Type', 'application/json']);
+  return res.send(response);
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const response = await Level.remove(id);
+
+  res.set(['Content-Type', 'application/json']);
+  return res.send(response);
+});
+
 module.exports = router;
--- backend/services/levels.service.js
+++ backend/services/levels.service.js
@@ -1,5 +1,5 @@
 const TABLE_NAME = 'levels';
-const ERROR = { error: 'No fue possible recuperar los datos'};
+const ERROR = { error: 'No fue possible recuperar los datos' };
 
 const Levels = {
   getAll: async () => {
@@ -11,6 +11,47 @@ const Levels = {
       console.log(e.message);
     }
     return response.length > 0 ? { response } : ERROR;
+  },
+  upsert: async (id, level, active) => {
+    let sql = '';
+    let error = { "error": "Error al ingresar/editar cargo" };
+    let operacion = '';
+
+    if (id) {
+      sql = `
+        UPDATE levels SET 
+	      level=?, active=?
+        WHERE ${id};
+      `;
+      operacion = 'update';
+    } else {
+      sql = `
+        INSERT INTO levels 
+	      (level, active)
+        VALUES (?, ?);
+      `;
+      operacion = 'insert';
+    }
+    try {
+      const response = await conn.query(sql, [level, active]);
+      return (response.changedRows || response.insertId)
+        ? { response: `Operación de ${operacion} realizada con éxito` }
+        : error;
+    } catch (e) {
+      console.log(e.message);
+      return error;
+    }
+  },
+  remove: async (id) => {
+    const sql = `DELETE FROM levels WHERE id = ?`;
+    let error = { "error": "Error al ingresar/editar level" };
+    try {
+      const response = await conn.query(sql, [id]);
+      return (response.changedRows === 1) ? { response: 'Removido con éxito' } : error;
+    } catch (e) {
+      console.log(e.message);
+      return error;
+    }
   },
 };
 
