deab0f7faa334fca151c09fda00df3272661881d
lucas-paz <lucas.hpaz@outlook.com>
2021-11-26 10:24:01 -0300

    Muestra info del puesto  feat: remaining endpoints to crud at careers table

--- backend/routes/careers.js
+++ backend/routes/careers.js
@@ -9,4 +9,32 @@ router.get('/', async (_req, res) => {
   return res.send(response);
 });
 
+router.put('/:id', async (req, res) => {
+  const { position, active } = req.body;
+  const { id } = req.params;
+
+  const response = await Career.upsert(id, position, active);
+
+  res.set(['Content-Type', 'application/json']);
+  return res.send(response);
+});
+
+router.post('/', async (req, res) => {
+  const { position, active } = req.body;
+
+  const response = await Career.upsert(null, position, active);
+
+  res.set(['Content-Type', 'application/json']);
+  return res.send(response);
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const response = await Career.remove(id);
+
+  res.set(['Content-Type', 'application/json']);
+  return res.send(response);
+});
+
 module.exports = router;
--- backend/services/careers.service.js
+++ backend/services/careers.service.js
@@ -1,5 +1,5 @@
 const TABLE_NAME = 'careers';
-const ERROR = { error: 'No fue possible recuperar los datos'};
+const ERROR = { error: 'No fue possible recuperar los datos' };
 
 const Career = {
   getAll: async () => {
@@ -11,6 +11,47 @@ const Career = {
       console.log(e.message);
     }
     return response.length > 0 ? { response } : ERROR;
+  },
+  upsert: async (id, position, active) => {
+    let sql = '';
+    let error = { "error": "Error al ingresar/editar cargo" };
+    let operacion = '';
+
+    if (id) {
+      sql = `
+        UPDATE careers SET 
+	      position=?, active=?
+        WHERE ${id};
+      `;
+      operacion = 'update';
+    } else {
+      sql = `
+        INSERT INTO careers 
+	      (position, active)
+        VALUES (?, ?);
+      `;
+      operacion = 'insert';
+    }
+    try {
+      const response = await conn.query(sql, [position, active]);
+      return (response.changedRows || response.insertId)
+        ? { response: `Operación de ${operacion} realizada con éxito`}
+        : error;
+    } catch (e) {
+      console.log(e.message);
+      return error;
+    }
+  },
+  remove: async (id) => {
+    const sql = `DELETE FROM careers WHERE id = ?`;
+    let error = { "error": "Error al ingresar/editar cargo" };
+    try {
+      const response = await conn.query(sql, [id]);
+      return (response.changedRows === 1) ? { response: 'Removido con éxito'} : error;
+    } catch (e) {
+     console.log(e.message);
+     return error;
+    }
   },
 };
 
