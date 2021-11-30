eb69968aac5897080e6482022f9913afaabc044e
lucas-paz <lucas.hpaz@outlook.com>
2021-11-30 10:40:17 -0300

    Migrate to vue clie - feat: implement vue toasted

--- vue-migration/src/main.js
+++ vue-migration/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
+import Toasted from 'vue-toasted';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+createApp(App)
+  .use(router)
+  .use(Toasted)
+  .mount('#app');
