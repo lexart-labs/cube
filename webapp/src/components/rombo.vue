c372af94ab62c0b55cd145482a4d03a792461412
lucas-paz <lucas.hpaz@outlook.com>
2021-12-30 16:44:23 -0300

    PL-22- feat-function to parse to percent evaluations

---
+++ webapp/src/components/rombo.vue
@@ -0,0 +1,30 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    parseEvaluation(evaluation, key) {
+      const MAX_VALUE = 5;
+
+      const valuesArray = evaluation.indicadores[key];
+      const maxTotal = valuesArray.length * MAX_VALUE;
+
+      const points = valuesArray.reduce((acc, cur, i) => {
+        acc += parseInt(cur.total);
+        return acc;
+      }, 0);
+
+      const result = (points * 100) / maxTotal;
+      return result;
+    },
+  },
+}
+</script>
