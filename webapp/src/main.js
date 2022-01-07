8ebf1d05df8fed9bfaafb326cf877570f5c35c8e
lucas-paz <lucas.hpaz@outlook.com>
2022-01-07 09:27:15 -0300

    PL-33-install i18n library

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import vSelect from 'vue-select';
 import Toasted from 'vue-toasted';
 import VueRouter from 'vue-router';
+import VueI18n from 'vue-i18n';
 import App from './App.vue';
 import router from './router';
 import 'vue-select/dist/vue-select.css';
@@ -10,6 +11,7 @@ Vue.config.productionTip = false;
 
 Vue.use(Toasted, { router });
 Vue.use(VueRouter);
+Vue.use(VueI18n);
 
 new Vue({
   router,
