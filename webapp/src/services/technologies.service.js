d1a203da150c177614aed1f281e4cfc176a93c6e
lucas-paz <lucas.hpaz@outlook.com>
2022-01-17 13:51:29 -0300

    pl-30 - fix - technologies services object logic

--- webapp/src/services/technologies.service.js
+++ webapp/src/services/technologies.service.js
@@ -9,27 +9,25 @@ const generateHeader = () => {
 };
 
 const headers = generateHeader();
+const MODEL = 'technologies/';
 
-const TechnologiesService = function () {
-  const MODEL = 'technologies/';
-  return {
-    getAll: async function () {
-      const { data } = await axios.get(`${API + MODEL}`, { headers });
-      return data;
-    },
-    getByUser: async function(idUser) {
-      const { data } = await axios.get(`${API}/skills/${idUser}`, { headers });
-      return data;
-    },
-    asignNew: async function (idUser, idTech) {
-      const {data} = await axios.post(`${API}/skills/${idUser}/${idTech}`, {}, { headers });
-      return data;
-    },
-    remove: async function (idUser, idTech) {
-      const {data} = await axios.delete(`${API}/skills/${idUser}/${idTech}`, {}, { headers });
-      return data;
-    },
-  };
+const TechnologiesService = {
+  getAll: async function () {
+    const { data } = await axios.get(`${API + MODEL}`, { headers });
+    return data;
+  },
+  getByUser: async function (idUser) {
+    const { data } = await axios.get(`${API}users/skills/${idUser}`, { headers });
+    return data;
+  },
+  asignNew: async function (idUser, idTech) {
+    const { data } = await axios.post(`${API}/skills/${idUser}/${idTech}`, {}, { headers });
+    return data;
+  },
+  remove: async function (idUser, idTech) {
+    const { data } = await axios.delete(`${API}/skills/${idUser}/${idTech}`, {}, { headers });
+    return data;
+  },
 };
 
 export default TechnologiesService;
