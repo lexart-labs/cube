7dd48a9580b4a41204323f2a60f9354ed0825c58
lucas-paz <lucas.hpaz@outlook.com>
2022-01-17 17:58:34 -0300

    pl-30 -feat - migration and seed

---
+++ db/seed_2022-01-17.sql
@@ -0,0 +1,11 @@
+INSERT INTO `technologies`
+  (`name`, `plataform`)
+VALUES
+  ('Vue JS', 'Web'),
+  ('React', 'Web'),
+  ('AngularJS', 'Web'),
+  ('NodeJS', 'Web'),
+  ('PHP', 'Web'),
+  ('Python', 'Web'),
+  ('React Native', 'Mobile'),
+  ('Flutter', 'Mobile')
