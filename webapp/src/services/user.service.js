0e718f896fd55c7bc53125e635fa709a19d391c1
lucas-paz <lucas.hpaz@outlook.com>
2022-02-02 08:07:07 -0300

    pl-44 - service - get dev indexes

--- webapp/src/services/user.service.js
+++ webapp/src/services/user.service.js
@@ -3,30 +3,30 @@ import axios from 'axios';
 
 import { APP_NAME, API } from '../../env';
 
+const buildHeaders = () => {
+  const token = localStorage.getItem(`token-app-${APP_NAME}`);
+  const userId = localStorage.getItem(`id-${APP_NAME}`);
+
+  const headers = {
+    token,
+    'user-id': userId,
+  };
+
+  return headers;
+};
+
 const UserService = function () {
   const model = 'users/';
   return {
     getUserById(id, cb) {
-      const token = localStorage.getItem(`token-app-${APP_NAME}`);
-      const userId = localStorage.getItem(`id-${APP_NAME}`);
-
-      const headers = {
-        token,
-        'user-id': userId,
-      };
+      const headers = buildHeaders();
 
       axios.get(API + model + id, { headers }).then((res) => {
         cb(res.data);
       });
     },
     getAllUsers(page, cb) {
-      const token = localStorage.getItem(`token-app-${APP_NAME}`);
-      const userId = localStorage.getItem(`id-${APP_NAME}`);
-
-      const headers = {
-        token,
-        'user-id': userId,
-      };
+      const headers = buildHeaders();
 
       const sql = page == null
         ? `${API + model}all`
@@ -37,66 +37,44 @@ const UserService = function () {
       });
     },
     getAllUsersLextracking(cb) {
-      const token = localStorage.getItem(`token-app-${APP_NAME}`);
-      const userId = localStorage.getItem(`id-${APP_NAME}`);
-
-      const headers = {
-        token,
-        'user-id': userId,
-      };
+      const headers = buildHeaders();
 
       axios.get(`${API + model}lextracking/all`, { headers }).then((res) => {
         cb(res.data);
       });
     },
     upsertUser(user, cb) {
-      const token = localStorage.getItem(`token-app-${APP_NAME}`);
-      const userId = localStorage.getItem(`id-${APP_NAME}`);
-
-      const headers = {
-        token,
-        'user-id': userId,
-      };
+      const headers = buildHeaders();
 
       axios.post(`${API + model}upsert`, user, { headers }).then((res) => {
         cb(res.data);
       });
     },
     getPagesLength(cb) {
-      const token = localStorage.getItem(`token-app-${APP_NAME}`);
-      const userId = localStorage.getItem(`id-${APP_NAME}`);
-
-      const headers = {
-        token,
-        'user-id': userId,
-      };
+      const headers = buildHeaders();
 
       axios.get(`${API + model}count`, { headers }).then((res) => {
         cb(res.data);
       });
     },
     getLeaders() {
-      const token = localStorage.getItem(`token-app-${APP_NAME}`);
-      const userId = localStorage.getItem(`id-${APP_NAME}`);
-
-      const headers = {
-        token,
-        'user-id': userId,
-      };
+      const headers = buildHeaders();
 
       return axios.get(`${API + model}leads`, { headers });
     },
     listLeadDevs() {
-      const token = localStorage.getItem(`token-app-${APP_NAME}`);
-      const userId = localStorage.getItem(`id-${APP_NAME}`);
-
-      const headers = {
-        token,
-        'user-id': userId,
-      };
+      const headers = buildHeaders();
 
       return axios.get(`${API + model}lead-tree`, { headers });
     },
+    allDevIndicators(year, cb) {
+      const headers = buildHeaders();
+
+      const endpoint = year
+        ? `${API + model}dev-indexes?year=${year}`
+        : `${API + model}dev-indexes`
+      axios.get(endpoint, { headers }).then(({ data }) => cb(data))
+    }
   };
 };
 
