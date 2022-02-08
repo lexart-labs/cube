561546d4df05173426121836efd9a32c63fb3fa9
lucas-paz <lucas.hpaz@outlook.com>
2022-02-08 08:20:02 -0300

    pl-41 - feat - turn dashboard into component

---
+++ webapp/src/components/DashboardComp.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <timeline :user="myUser" v-if="myUser" />
+    <h4 class="text-center" v-if="years.length === 0">
+      {{ translations[$store.state.language].dashboard.userHaventEvaluations }}
+    </h4>
+    <div class="graphics-ctl">
+      <graphic v-if="evaluations.length" :evaluations="evaluations" />
+      <Rombo v-if="evaluations.length" :evaluations="evaluations" :year="year" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Timeline from './Timeline.vue';
+import translations from '../data/translate';
+import graphic from './graphicEvaluation.vue';
+import Rombo from './rombo.vue';
+
+export default {
+  name: "DashComp",
+  // props: { 'dev', }
+  components: { Timeline, graphic, Rombo },
+  data() {
+    return {
+      myUser: {},
+      years: [],
+      evaluations: [],
+      year: [],
+    };
+  },
+  watch: {
+    "$store.state.language": function (newVal, oldVal) {
+      this.success = this.success
+        ? translations[this.$store.state.language].dashboard.messageSyncStatus
+        : translations[this.$store.state.language].dashboard.messageNotSync;
+    },
+  },
+  methods: {},
+  methods: {},
+  mounted() {},
+  computed: {},
+};
+</script>
+
+<style scoped>
+</style>
