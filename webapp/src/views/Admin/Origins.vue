5425465aeab6d2771df3a4a713f008fc96e933aa
lucas-paz <lucas.hpaz@outlook.com>
2022-02-18 11:25:25 -0300

    pl-67 - feat - new tab origins logics

---
+++ webapp/src/views/Admin/Origins.vue
@@ -0,0 +1,181 @@
+<template>
+  <div>
+    <h4 class="courseTitle">
+      <span>{{ $t("AdminOrigins.title") }}</span>
+      <spinner v-if="isLoading"></spinner>
+    </h4>
+
+    <div class="row" style="gap: 1rem; margin: 1rem auto 3rem;">
+      <input
+        type="text"
+        v-model="newPlataform.plataform"
+        :placeholder="$t('AdminOrigins.placeholder')"
+        class="form-control col-8"
+      />
+      <button
+        type="button"
+        class="btn btn-primary col-1"
+        v-on:click="isEditing ? updatePlataform() : addPlataform()"
+        :disabled="!newPlataform.plataform"
+      >
+        {{ $t("generic.save") }}
+      </button>
+    </div>
+
+    <div class="courseContainer">
+      <table class="table">
+        <thead>
+          <tr>
+            <th
+              v-for="(header, i) in $t('AdminOrigins.tableHeaders')"
+              :key="`head${i}`"
+            >
+              {{ header }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, i) in technologies" :key="`tech${i}`">
+            <td>{{ item.id }}</td>
+            <td>{{ item.plataform }}</td>
+            <td style="display: flex; gap: 1rem;">
+              <button class="btn btn-info" data-toggle="modal" v-on:click="setEditing(item)">
+                {{ $t("generic.edit") }}
+              </button>
+              <button class="btn btn-danger" data-toggle="modal" v-on:click="deletePlataform(item.id)">
+                {{ $t("generic.remove") }}
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div v-if="isLoading" class="loading-cover">
+      <Spinner />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Spinner from "../../components/Spinner.vue";
+import { APP_NAME } from "../../../env";
+import translations from '../../data/translate';
+import DevOriginsService from '../../services/devOrigins.service';
+
+const DEFAULT_VALUE = {
+  id: 0,
+  plataform: '',
+};
+
+export default {
+  name: "Origin",
+  components: { Spinner },
+  data() {
+    return {
+      isEditing: false,
+      isLoading: false,
+      error: '',
+      plataforms: [],
+      newPlataform: { ...DEFAULT_VALUE},
+      token: localStorage.getItem(`token-app-${APP_NAME}`),
+    };
+  },
+  methods: {
+    getPlataforms: async function () {
+      this.isLoading = true;
+
+      this.plataforms = await DevOriginsService.getAll();
+
+      this.isLoading = false;
+    },
+    updatePlataform: async function() {
+      this.isLoading = true;
+      this.isEditing = false;
+      const { id, plataform } = this.newPlataform;
+
+      const { data } = await DevOriginsService.update(id, plataform);
+      
+      if(data.response) {
+        await this.getPlataforms();
+        this.newPlataform = {...DEFAULT_VALUE};
+        Vue.toasted.show(translations[this.$store.state.language].AdminTechnologies.success, {
+            type: "success",
+            duration: 2000,
+        });
+      } else {
+        this.isLoading = false;
+        this.error = data.error;
+        Vue.toasted.show(translations[this.$store.state.language].AdminTechnologies.error, {
+            type: "error",
+            duration: 2000,
+        });
+      }
+    },
+    deletePlataform: async function(id) {
+      this.isLoading = true;
+
+      const { data } = await DevOriginsService.remove(id);
+      
+      if(data.response) {
+        await this.getPlataforms();
+        Vue.toasted.show(translations[this.$store.state.language].AdminTechnologies.success, {
+            type: "success",
+            duration: 2000,
+        });
+      } else {
+        this.isLoading = false;
+
+        this.error = data.error;
+        Vue.toasted.show(translations[this.$store.state.language].AdminTechnologies.error, {
+            type: "error",
+            duration: 2000,
+        });
+      }
+    },
+    addPlataform: async function() {
+      this.isLoading = true;
+
+      const {data} = await DevOriginsService.insert(this.newPlataform.plataform);
+      
+      if(data.response) {
+        await this.getPlataforms();
+        this.newPlataform = {...DEFAULT_VALUE};
+        Vue.toasted.show(translations[this.$store.state.language].AdminTechnologies.success, {
+            type: "success",
+            duration: 2000,
+        });
+      } else {
+        this.isLoading = false;
+        Vue.toasted.show(translations[this.$store.state.language].AdminTechnologies.error, {
+            type: "error",
+            duration: 2000,
+        });
+      }
+    },
+    setEditing(plataform) {
+      this.isEditing = true;
+      this.newPlataform = {...plataform};
+    },
+  },
+  mounted: async function () {
+    this.getPlataforms();
+  },
+};
+</script>
+
+<style scoped>
+  .courseContainer {
+    display: flex;
+    justify-content: center;
+    height: 50vh;
+    overflow-y: scroll;
+    box-shadow: rgba(0, 0, 0, 0.109) 0px 2px 4px 0px inset;
+  }
+
+  h4 {
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+  }
+</style>
