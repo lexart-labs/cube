276f1dd9f50df69314afcf7396618ab60844d880
lucas-paz <lucas.hpaz@outlook.com>
2022-02-18 09:13:10 -0300

    pl-67 - feat - routes

---
+++ backend/routes/devOrigins.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const DevOrigins = require('../services/DevOrigins.service');
+
+router.get('/', async function (_req, res) {
+  const response = await DevOrigins.getAll();
+
+  res.set(['Content-Type', 'application/json']);
+    res.send(response);
+});
+
+router.get('/:id', async function (req, res) {
+  const { id } = req.params;
+
+  const response = await DevOrigins.getById(id);
+
+  res.set(['Content-Type', 'application/json']);
+    res.send(response);
+});
+
+router.post('/', async function (req, res) {
+  const response = await DevOrigins.insert(req.body);
+
+  res.set(['Content-Type', 'application/json']);
+    res.send(response);
+});
+
+router.put('/:id', async function (req, res) {
+  const { id } = req.params;
+
+  const response = await DevOrigins.update(id, req.body);
+
+  res.set(['Content-Type', 'application/json']);
+    res.send(response);
+});
+
+router.delete('/:id', async function (req, res) {
+  const { id } = req.params;
+
+  const response = await DevOrigins.remove(id);
+
+  res.set(['Content-Type', 'application/json']);
+    res.send(response);
+});
+
+module.exports = router;
