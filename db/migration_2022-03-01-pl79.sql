32b37a26cb7a6835fcef634820a6416d8aae7d8a
lucas-paz <lucas.hpaz@outlook.com>
2022-03-01 09:13:46 -0300

    pl-79 - feat - migration

---
+++ db/migration_2022-03-01-pl79.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `lexart_cube`.`colaborators_continuity` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `month` INT(2) NOT NULL,
+  `year` INT(4) NOT NULL,
+  `continuity` INT NOT NULL,
+  `idColaborator` INT NOT NULL,
+  `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+ALTER TABLE colaborators_continuity ADD FOREIGN KEY (idColaborator) REFERENCES users(id);
