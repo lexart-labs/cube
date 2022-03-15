469f7bcbb1f516f94f173a29c85d80fe80272685
Lexart Dev <developer@lexartlabs.com>
2022-03-15 13:55:39 -0300

    Redirect to /:slug/login when logout

--- webapp/src/components/Menu.vue
+++ webapp/src/components/Menu.vue
@@ -54,7 +54,7 @@
                 </li>
                 <li>
                   <router-link
-                    v-bind:to="setting.token ? '/' + setting.token : '/'"
+                    v-bind:to="`/${slug}/login`"
                   ><i class="bi bi-box-arrow-left"></i>
                     <small> {{ $t("generic.exit")}}</small>
                   </router-link>
