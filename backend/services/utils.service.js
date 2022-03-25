dd2d53fc3bd266835900fcc7c650af17a462843b
lucas-paz <lucas.hpaz@outlook.com>
2022-03-25 13:34:57 -0300

    pl-105 - feat - send email

--- backend/services/utils.service.js
+++ backend/services/utils.service.js
@@ -1,14 +1,15 @@
-global.md5 		  = require('md5');
+global.md5 = require('md5');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const sha1 = require('locutus/php/strings/sha1');
+const axios = require('axios');
 
 const Utils = {
 	makeToken: function (usr) {
 		const jwtConfig = {
-      expiresIn: '7d',
-      algorithm: 'HS256',
-    };
+			expiresIn: '7d',
+			algorithm: 'HS256',
+		};
 		const secret = process.env.SECRET;
 		const token = jwt.sign({ data: usr }, secret, jwtConfig);
 		return token;
@@ -21,6 +22,35 @@ const Utils = {
 		} catch (error) {
 			res ? res.send({ status: 404, message: "This company doesn't exists" }) : '';
 		}
-	}
+	},
+	sendEmail: async (payload) => {
+		console.log('sending email');
+		const { email, message, topic } = payload;
+		const endpoint = 'https://mail-ssl.lexartlabs.uy/mail/smtp/custom/new';
+		const headers = { Authorization: process.env.LX_MAIL_AUTH };
+		const body = {
+			site_title: "Cube",
+			topic,
+			to_email: 'luukaspaz031@gmail.com',
+			headers: {
+				from_email: 'sendmail01@lexart-mail-api2.com',
+				bcc_emails: 'cube@lexartlabs.com',
+			},
+			body: {
+				slogan: '#Cube. Go further',
+				big_logo: '',
+				little_logo: '',
+				html_body: message,
+				footer_one: '',
+				footer_two: '',
+				image_head: 'no',
+				image_head_little: 'no',
+				image_footer: 'no',
+				footer_color: "#000000;color:#ff0000db !important;font-size: 10px"
+			}
+		};
+		const { data } = await axios.post(endpoint, body, { headers });
+		return data;
+	},
 }
 module.exports = Utils;
