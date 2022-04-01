ec88fc85a395e62f504a561a77d68ca03154f1ac
João <vitorjao56@gmail.com>
2022-04-01 13:32:08 -0300

    feat: migration create to add columns fk

--- db/migration_2022-03-31.sql
+++ db/migration_2022-03-31.sql
@@ -1,3 +1,8 @@
+
 ALTER TABLE levels
   ADD COLUMN idCompany NOT NULL INT(11) DEFAULT(1),
-  ADD FOREIGN KEY fk_id_companies_levels REFERENCES companies(id) ON DELETE CASCADE;
+  ADD CONSTRAINT  fk_id_companies_levels FOREIGN KEY (idCompany) REFERENCES companies(id) ON DELETE CASCADE;
+
+ALTER TABLE levels
+  ADD COLUMN idCareerType NOT NULL INT(11) DEFAULT(1),
+  ADD CONSTRAINT fk_id_levels_career_type FOREIGN KEY (idCareerType) REFERENCES careers_type(id) ON DELETE CASCADE;
