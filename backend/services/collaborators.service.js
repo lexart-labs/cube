04c47bdaf911cec0793ee40c2a47f3dddccf8c9b
Lexart Dev <developer@lexartlabs.com>
2022-04-01 10:45:59 -0300

    Change text 'Welcomo' to 'Welcome'

--- backend/services/collaborators.service.js
+++ backend/services/collaborators.service.js
@@ -93,7 +93,7 @@ const Collaborators = {
       <hr>
       <br>
       <p>
-        Welcomo to cube plataform! Below you have the email and password to access your account,
+        Welcome to cube plataform! Below you have the email and password to access your account,
         as well as the link to your company's login page;
       </p>
       <p><b>Company Name: </b> ${company[0].company || 'not found'}</p>
