324d3dd0959f302e2df4bf004825e38ecec5d45e
Lexart Dev <developer@lexartlabs.com>
2022-04-26 23:00:31 -0300

    Dashboard timeline # feat: Fix the graphic of positions in the dashboard, take the position of the API

--- db/migration_2022-03-31.sql
+++ db/migration_2022-03-31.sql
@@ -1,10 +1,10 @@
 
 ALTER TABLE levels
-  ADD COLUMN idCompany NOT NULL INT(11) DEFAULT(1),
+  ADD COLUMN idCompany INT(11) NOT NULL DEFAULT(1),
   ADD CONSTRAINT  fk_id_companies_levels FOREIGN KEY (idCompany) REFERENCES companies(id) ON DELETE CASCADE;
 
 ALTER TABLE levels
-  ADD COLUMN idCareerType NOT NULL INT(11) DEFAULT(1),
+  ADD COLUMN idCareerType INT(11) NOT NULL DEFAULT(1),
   ADD CONSTRAINT fk_id_levels_career_type FOREIGN KEY (idCareerType) REFERENCES careers_type(id) ON DELETE CASCADE;
 
 ALTER TABLE levels 
